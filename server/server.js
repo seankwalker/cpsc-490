bf9cc3b0d55d3d55ea677f6fa718c1912a7adb64
Sean Walker <sean.walker@yale.edu>
2019-04-29 13:26:45 -0400

    WIP: add server functionality to parse config files

--- server/server.js
+++ server/server.js
@@ -1,22 +1,19 @@
 import express from "express";
 import multer from "multer";
+import fs from "fs";
 
 // port to run server on
 const PORT = 3000;
 
 /* FILE UPLOAD SETUP */
 
-/*
- * Return `true` if `f` is a JSON file, else false.
- */
+// filter out non-JSON file extensions
 const jsonFileFilter = (req, f, cb) => {
     let valid = f.originalname.substring(f.originalname.length - 5) === ".json";
     cb(null, valid);
 };
 
-/*
- * Save files as "`Date.now()`-filename.json"
- */
+// save files with their original name
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "uploads/");
@@ -25,6 +22,25 @@ const storage = multer.diskStorage({
         cb(null, Date.now() + "-" + file.originalname);
     }
 });
+
+/* CONFIG FILE PARSING */
+
+// open a config file and send it to JSON parser
+const openFile = filePath => {
+    fs.readFile(filePath, "utf-8", (err, contents) =>
+        parseConfigFile(contents)
+    );
+};
+
+// parse config file into JS object
+const parseConfigFile = contents => {
+    const parsed = JSON.parse(contents);
+    console.log("pared JSON object:", parsed);
+
+    // initialize a Docker container with a service chain respecting the
+    // directed graph specified in the config file
+    // TODO
+};
 
 /* ROUTING SETUP */
 
@@ -37,6 +53,7 @@ app.use(express.json());
 app.post("/start", upload.single("data"), (req, res, next) => {
     console.log("config file saved as:", req.file.filename);
     res.sendStatus(200);
+    openFile(req.file.path);
 });
 
 // listen for requests
